1e69bfbfcfa5de98a2e61be5555240e934e51355
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-08-27 15:47:02 +0200

    :sparkles: adds animal loader to routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Animals from "./pages/Animals";
 import Layout from "./pages/Layout";
+import Animal from "./pages/Animal";
+import { animalLoader } from "./loaders/animalLoader";
 
 export const router = createBrowserRouter([
   {
@@ -15,7 +17,12 @@ export const router = createBrowserRouter([
       {
         path: "/animals",
         element: <Animals></Animals>
+      },
+      {
+        path: "/animals/:id",
+        element: <Animal />,
+        loader: animalLoader,
       }
     ]
-  },  
+  }
 ])
