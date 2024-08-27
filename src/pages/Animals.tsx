d7982f069e31aad545bfdddfc771294cfe80a5d3
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-08-27 15:48:33 +0200

    :sparkles: adds states

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,9 +1,45 @@
+import { IAnimal } from "../models/IAnimal"
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { get } from "../services/animalService";
+ 
+  const BASE_URL = "https://animals.azurewebsites.net/api/animals";
 
+  const Animals = () => {
+    const [animals, setAnimals] = useState<IAnimal[]>([]);
+    const [fetched, setFetched] = useState(false); 
+  
+    const getData = async () => {
+      try {
+        const response = await get<IAnimal[]>(`${BASE_URL}`);
+        setAnimals(response.data); 
+        setFetched(true);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  
+    useEffect(() => {
+      if (fetched) return; 
+      getData(); 
+    })
 
-const Animals = () => {
   return (
     <>
       <h2>Dina fantastiska djur:</h2>
+      <section className="animals">
+        {animals.map((animal) => (
+          <div key={animal.id} className="animal-presentation">
+            <h3 className="animal-name">{animal.name}</h3>
+            <div className="animal-image-container">
+              <img src={animal.imageUrl} alt={animal.name} className="animal-image" />
+            </div>
+            <div className="animal-description">{animal.shortDescription}</div>
+            <Link to={`/animals/${animal.id}`} className="animal-button-link"><button className="animal-button">Se mer</button></Link>
+          </div>
+         
+        ))}
+      </section>
     </>
     
   )
