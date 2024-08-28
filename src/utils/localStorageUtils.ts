9e793cb036e3a5d1bd3cd48160e6853cea3f3723
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-08-28 09:25:58 +0200

    :sparkles: adds utils for localstorage

---
+++ src/utils/localStorageUtils.ts
@@ -0,0 +1,7 @@
+export const updateLocalStorage = (key: string, value: string) => {
+  localStorage.setItem(key, value);
+};
+
+export const getFromLocalStorage = (key: string) => {
+  return localStorage.getItem(key);
+};
