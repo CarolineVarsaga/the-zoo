816537ee57036c2f12a17219d9a0c1852717e277
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-08-28 09:25:42 +0200

    :sparkles: adds feed function

--- src/services/animalService.ts
+++ src/services/animalService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { updateLocalStorage } from "../utils/localStorageUtils";
 
 const BASE_URL = "https://animals.azurewebsites.net/api/animals";
 
@@ -9,3 +10,15 @@ export const get = async <T>(url: string) => {
 export const getAnimal = async (id: number) => {
   return await get(`${BASE_URL}/${id}`);
 }
+
+export const feedAnimal = (animalId: number) => {
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString([], { month: '2-digit', day: '2-digit', year: 'numeric' });
+  const formattedTime = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+  const datetime = `${formattedDate}, kl: ${formattedTime}`;
+
+  updateLocalStorage(`lastFed-${animalId}`, datetime);
+
+  return datetime;
+};
