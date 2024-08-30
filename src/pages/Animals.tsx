7c2197c2d24736c973013dea4481a4f1ec8fa9ab
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-08-30 16:06:02 +0200

    :sparkles: imports custom hook

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,43 +1,12 @@
-import { IAnimal } from "../models/IAnimal"
-import { useState, useEffect } from "react";
-import { get } from "../services/animalService";
-import { getFromLocalStorage, updateLocalStorage } from "../utils/localStorageUtils";
 import ShowAnimals from "../components/ShowAnimals";
- 
-const BASE_URL = "https://animals.azurewebsites.net/api/animals";
+import useAnimals from "../hooks/useAnimals";
 
 const Animals = () => {  
-  const [animals, setAnimals] = useState<IAnimal[]>([]);
-  const [fetched, setFetched] = useState(false); 
-  const storedAnimals = getFromLocalStorage("animals");
+  const { animals, error, fetched } = useAnimals();
+
+  if (!fetched) return <p>Hämtar djuren...</p>;
+  if (error) return <p>{error}</p>;
   
-  const getData = async () => {
-    try {
-      const response = await get<IAnimal[]>(`${BASE_URL}`);
-      const data = response.data;
-      setAnimals(data); 
-      updateLocalStorage("animals", JSON.stringify(data));
-      setFetched(true);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }
-
-  const getAnimalsFromLocalStorage = () => {
-    if (storedAnimals) {
-      const parsedAnimals = JSON.parse(storedAnimals);
-      setAnimals(parsedAnimals);
-      setFetched(true);
-    } else {
-      getData();
-    }
-  };
-
-  useEffect(() => {
-    if (fetched) return; 
-    getAnimalsFromLocalStorage(); 
-  })
-
   return (
     <>
       <ShowAnimals animals={animals}/>
