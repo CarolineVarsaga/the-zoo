37b12caf0b322f5f2e5a3dfaa5ade5023e5455c7
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-09-02 10:14:45 +0200

    :fire: remove app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router'
-import './App.css'
-
 
 function App() {
-
   return (
     <>   
       <RouterProvider router={router}></RouterProvider>
-      
     </>
   )
 }
